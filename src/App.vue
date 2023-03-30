229b24a6e396817785f64406b53fd9fc3a35259b
Johan van Tongeren <johan.van-tongeren@postnord.com>
2023-03-30 14:23:18 +0200

    chore: Publish latest version

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,107 @@
       </div>
       <div class="section">
         <div class="slide">
-          <h3>Slide 2.1</h3>
+          <h3>Top products used</h3>
         </div>
         <div class="slide">
-          <h3>Slide 2.2</h3>
-        </div>
-        <div class="slide">
-          <h3>Slide 2.3</h3>
+          <h3>Varubrev 🎉</h3>
+          <h4>10.183 parcels</h4>
         </div>
       </div>
       <div class="section">
-        <h3>Section 3</h3>
+        <div class="slide">
+          <h3>Lightning fast delivery</h3>
+        </div>
+        <div class="slide">
+          <h3>From order to pickup time</h3>
+          <p>Average: 1 day, 6 hours and 5 minutes</p>
+        </div>
+        <div class="slide">
+          <h3>Average pickup time</h3>
+          <p>
+            How long does it take for receivers to pickup their parcels from a
+            service point or parcel locker: 12 hours and 8 minutes
+          </p>
+        </div>
+        <div class="slide">
+          <h3>Fastest pickup time</h3>
+          <p>
+            From getting the notification to picking up the parcel: 18 minutes,
+            3 seconds and 150 milliseconds
+          </p>
+        </div>
+      </div>
+      <div class="section">
+        <div class="slide">
+          <h3>How did we help the environment?</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            maximus, nunc ut laoreet tincidunt, metus nulla gravida massa, vel
+            imperdiet ante ligula id velit. Cras commodo ornare porta. Quisque
+            luctus tortor a sapien congue, ac consequat metus euismod. Cras arcu
+            enim, tincidunt eu dolor at, faucibus consequat magna. Ut quis
+            sodales est, non pellentesque felis. Praesent enim nibh, rutrum
+            interdum maximus ut, hendrerit id ipsum.
+          </p>
+        </div>
+        <div class="slide">
+          <h3>🌱 511.423 kg of CO2 saved</h3>
+          <p>
+            Our fossilfree delivery options have saved 511.423 kilos of CO2.
+          </p>
+        </div>
+        <div class="slide">
+          <h3>🚙 389 new electric vehicles</h3>
+          <p>
+            We got rid of 389 fossil fuel vehicles and we replaced them with
+            fossil free vehicles.
+          </p>
+        </div>
+      </div>
+      <div class="section">
+        <div class="slide">
+          <h3>What do people think about us?</h3>
+          <p>Some of the feedback we got:</p>
+          <h4>"I love postnord. I would like to dy my hair blue! - Johan</h4>
+          <h4>"Postnord is the greatest thing since sliced bread! - Anna</h4>
+        </div>
+        <div class="slide">
+          <h3>Service point</h3>
+          <p>Average score: 4.3 / 5 ⭐️⭐️⭐️⭐️</p>
+          <h3>Varubrev</h3>
+          <p>Average score: 5 / 5 ⭐️⭐️⭐️⭐️⭐️</p>
+        </div>
+      </div>
+      <div class="section">
+        <div class="slide">
+          <h3>Where are your customers?</h3>
+          <img
+            src="https://community.qlik.com/legacyfs/online/35405_heatmap.PNG"
+            alt=""
+          />
+        </div>
+        <div class="slide">
+          <h3>Service point</h3>
+          <p>Average score: 4.3 / 5 ⭐️⭐️⭐️⭐️</p>
+          <h3>Varubrev</h3>
+          <p>Average score: 5 / 5 ⭐️⭐️⭐️⭐️⭐️</p>
+        </div>
+      </div>
+      <div class="section">
+        <div class="slide">
+          <h3>What are we going to do?</h3>
+          <p>
+            We don't sit still! Next year we're going to keep innovating. This
+            means this for your business
+          </p>
+        </div>
+        <div class="slide">
+          <h3>New fossilfree corridor to Örebro 🔴⚪️</h3>
+          <p>
+            This means that all of your 3001 customers in the Örebro län will
+            save up to 30.000kg of CO2
+          </p>
+        </div>
       </div>
     </full-page>
   </div>
@@ -64,11 +154,18 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-.section {
+.section,
+.slide {
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+.section p {
+  display: inline-block;
+  max-width: 800px;
+  margin-top: 30px;
+  margin-bottom: 20px;
 }
 a {
   color: #42b983;
